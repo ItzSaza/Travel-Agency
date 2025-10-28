f820786a58d2ab4f98bfc2cebe6d49d727106f55
muhammedd-aslam <muhammeddaslam29@gmail.com>
2025-10-28 12:17:48 +0530

    rfrfrfff

--- add_hotel.php
+++ add_hotel.php
@@ -5,7 +5,7 @@ header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Accept');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    // CORS preflight
+
     http_response_code(200);
     exit;
 }
@@ -22,7 +22,7 @@ if (stripos($contentType, 'application/json') !== false && $raw) {
         exit;
     }
 } else {
-    // Fallback to form-encoded POST
+    
     $input = $_POST;
 }
 
