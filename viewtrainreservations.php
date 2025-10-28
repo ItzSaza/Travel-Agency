9126b130f2ff8871e5256e2ac903334470056d6f
BihanDoo <sathmirabihandu@gmail.com>
2025-10-28 10:40:44 +0530

    commmmmm

--- viewtrainreservations.php
+++ viewtrainreservations.php
@@ -3,10 +3,6 @@ header('Content-Type: application/json');
 
 $jsonFile = 'trainreservations.json';
 
-if (!file_exists($jsonFile)) {
-    echo json_encode(['success' => false, 'message' => 'No reservations found']);
-    exit;
-}
 
 $jsonContent = file_get_contents($jsonFile);
 $data = json_decode($jsonContent, true);
@@ -18,6 +14,12 @@ if (isset($_GET['train'])) {
         return strcasecmp($reservation['trainName'], $trainName) === 0;
     });
     echo json_encode(['success' => true, 'reservations' => array_values($filtered)]);
+} elseif (isset($_GET['id'])) { //searching by ID
+    $reservationId = $_GET['id'];
+    $filtered = array_filter($data['reservations'], function($reservation) use ($reservationId) {
+        return $reservation['reservationId'] == $reservationId;
+    });
+    echo json_encode(['success' => true, 'reservation' => array_values($filtered)[0] ?? null]);
 } else {
     echo json_encode(['success' => true, 'reservations' => $data['reservations']]);
 }
